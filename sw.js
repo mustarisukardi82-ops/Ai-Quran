ccbedc096f946a815271f29dc734a35b04625b3c
mustarisukardi82-ops <mustari.sukardi82@gmail.com>
2025-12-15 14:31:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
 self.addEventListener("fetch",()=>{});
+const CACHE="ai-quran-v1";
+const ASSETS=[
+  "./","./index.html","./style.css","./app.js","./quran-loader.js"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return res;
+    }))
+  );
+});
