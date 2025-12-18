eb6ef063f1d0cad45dbf3b0f30c28e6d0864c468
mustarisukardi82-ops <mustari.sukardi82@gmail.com>
2025-12-18 16:20:43 +0800

    Disable service worker cache

--- sw.js
+++ sw.js
@@ -1,19 +1,7 @@
-self.addEventListener("fetch",()=>{});
-const CACHE="ai-quran-v1";
-const ASSETS=[
-  "./","./index.html","./style.css","./app.js","./quran-loader.js"
-];
-
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-      const clone=res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,clone));
-      return res;
-    }))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
